5e21181b34e52b08ad3102288263a9de9adbfd8e
themanaspatra <themanaspatra@gmai.com>
2025-06-07 16:46:53 +0530

    Program for python

--- pythonframework/pytest_concepts/test_sample.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.mark.dependency()
-def test_case_1():
-    assert 'hello' == 'hai'
-
-@pytest.mark.dependency(depends=['test_case_1'])
-def test_case_2():
-    assert 1 == 3
--- pythonframework/utilities/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-
-    URL = "https://demowebshop.tricentis.com/"
-    BROWSER = "chrome"
-
-    SCREEN_SHOT = r"C:\Users\patra\Desktop\Practice\pythonframework\screen_shots\\"
